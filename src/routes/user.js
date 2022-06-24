8d0717109b78489f08d3285914745ca9fac34b57
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-24 12:08:38 -0300

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -29,7 +29,29 @@ router.get("/rifa", (req, res) => {
         .catch((error) => res.json({ message: "Erro na busca de um agendamento. Entre em contado com o desenvolvedor" }));
 });
 
+//mexi daqui pra baixo
 
+
+// delete a user
+router.delete("/rifa/:id", (req, res) => {
+    const { id } = req.params;
+    userSchema
+        .deleteOne({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+// update a user
+router.put("/rifa/:id", (req, res) => {
+    const { id } = req.params;
+    const { numero, nome} = req.body;
+    userSchema
+        .updateOne({ _id: id }, { $set: { numero, nome} })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//ate aqui
 
 
 module.exports = router;
